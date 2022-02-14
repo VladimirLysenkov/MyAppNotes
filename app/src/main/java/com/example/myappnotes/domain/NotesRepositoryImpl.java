231ae7d96ae32f6819783a676ff3d849b58f5589
vladimir_lysenkov <vladimirv.lysenkov@gmail.com>
2022-02-14 21:28:24 +0500

    Создал фрагмент для деталей заметки, активити и макет. Но пока приложение падает при открытии деталей заметки.. java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.myappnotes/com.example.myappnotes.ui.MainActivity}: java.lang.ClassCastException: java.util.Date cannot be cast to java.lang.CharSequence Пока не понял в чём причина и не починил.
    Поля даты создания и даты изменения заметки также не перевел в тип данных Date. Не разобрбался как их опеределить и вывести..

--- app/src/main/java/com/example/myappnotes/domain/NotesRepositoryImpl.java
+++ app/src/main/java/com/example/myappnotes/domain/NotesRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
 public class NotesRepositoryImpl implements NotesRepository {
 
     private static final NotesRepository INSTANCE = new NotesRepositoryImpl();
@@ -16,9 +17,9 @@ public class NotesRepositoryImpl implements NotesRepository {
     public List<Note> getNotes() {
         ArrayList<Note> notes = new ArrayList<>();
 
-        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №1", "Описание заметки №1", "09.02.2022", "10.02.2022", "Текст заметки №1"));
-        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №2", "Описание заметки №2", "04.02.2022", "09.02.2022", "Текст заметки №2"));
-        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №3", "Описание заметки №3", "22.12.2021", "01.02.2022", "Текст заметки №3"));
+        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №1", "Описание заметки №1", "11.12.21", "01.12.21", "Текст заметки №1"));
+        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №2", "Описание заметки №2", "22.12.21", "02.12.21", "Текст заметки №2"));
+        notes.add(new Note(UUID.randomUUID().hashCode(), "Заметка №3", "Описание заметки №3", "01.12.21", "03.12.21", "Текст заметки №3"));
 
         return notes;
     }
--- app/src/main/java/com/example/myappnotes/ui/details/NoteDetailsFragment.java
+++ app/src/main/java/com/example/myappnotes/ui/details/NoteDetailsFragment.java
@@ -10,23 +10,55 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myappnotes.R;
+import com.example.myappnotes.domain.Note;
 
 
 public class NoteDetailsFragment extends Fragment {
+
+    private static final String ARG_NOTE = "ARG_NOTE";
+
+    public static NoteDetailsFragment newInstance(Note note) {
+
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_NOTE, note);
+
+        NoteDetailsFragment fragment = new NoteDetailsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 
     public NoteDetailsFragment() {
         super(R.layout.fragment_note_details);
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextView name = view.findViewById(R.id.name_note);
+        Bundle arguments = getArguments();
+
+        assert arguments != null;
+        Note note = arguments.getParcelable(ARG_NOTE);
+
+        TextView name = view.findViewById(R.id.name);
+        name.setText(note.getName());
+
+
         TextView dateCreated = view.findViewById(R.id.date_created);
+        dateCreated.setText(note.getCreatedAt());
+
         TextView dateUpdated = view.findViewById(R.id.date_updated);
+        dateUpdated.setText(note.getUpdatedAt());
+
         TextView discription = view.findViewById(R.id.discription);
+        discription.setText(note.getDescription());
+
         TextView noteText = view.findViewById(R.id.note_text);
+        noteText.setText(note.getINoteText());
+
 
     }
+
 }
